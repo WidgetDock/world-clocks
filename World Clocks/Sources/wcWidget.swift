cb3c90b004e166c9b56448331165837096a2e5ad
wfxey <devinoldenburg16@gmail.com>
2025-06-21 12:48:54 +0200

    Simple Widget / CANT CHANGE TIMEZONES YET

--- World Clocks/Sources/wcWidget.swift
+++ World Clocks/Sources/wcWidget.swift
@@ -1,27 +1,62 @@
-//
-//  wcWidget.swift
-//  World Clocks
-//
-//  Created by Devin on 21.06.25.
-//  
-//
-
 import Foundation
 import AppKit
 import PockKit
 
 class wcWidget: PKWidget {
-    
-    static var identifier: String = "widgetdock.World-Clocks"
-    var customizationLabel: String = "World Clocks"
-    var view: NSView!
-    
+    static let identifier: String = "widgetdock.World‑Clocks"
+    var customizationLabel: String {
+        get { "World Clocks" }
+        set { /* optionally handle set */ }
+    }
+
+    private var timeZones: [TimeZone] = [.current, TimeZone(secondsFromGMT: 0)!]
+    private let stack = NSStackView()
+    var view: NSView! {
+        get { stack }
+        set { /* optionally handle set */ }
+    }
+
     required init() {
-        self.view = PKButton(title: "World Clocks", target: self, action: #selector(printMessage))
+        buildUI()
+        view = stack
+        updateTimes()
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
+            self?.updateTimes()
+        }
     }
-    
-    @objc private func printMessage() {
-        NSLog("[wcWidget]: Hello, World!")
+
+    private func buildUI() {
+        stack.orientation = .vertical
+        stack.spacing = 4
+
+        for _ in 0..<2 {
+            let label = NSTextField(labelWithString: "--:--:--")
+            label.alignment = .center
+            label.font = .monospacedDigitSystemFont(ofSize: 10, weight: .medium)
+            stack.addArrangedSubview(label)
+        }
     }
-    
+
+    @objc private func saveSettings(_ sender: NSButton) {
+        guard let stack = sender.superview as? NSStackView else { return }
+        let combos = stack.arrangedSubviews.compactMap { $0 as? NSComboBox }
+        for (i, cb) in combos.enumerated() {
+            if let tz = TimeZone(identifier: cb.stringValue) {
+                timeZones[i] = tz
+            }
+        }
+        NSApp.stopModal()
+        updateTimes()
+    }
+
+    private func updateTimes() {
+        let date = Date()
+        for (index, view) in stack.arrangedSubviews.enumerated() {
+            guard let label = view as? NSTextField, index < timeZones.count else { continue }
+            let formatter = DateFormatter()
+            formatter.timeZone = timeZones[index]
+            formatter.dateFormat = "HH:mm:ss"
+            label.stringValue = formatter.string(from: date)
+        }
+    }
 }
